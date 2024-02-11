b2831585e6bc9d7e9e6593c531ae7f5ce14cc3bd
Poleszak <poleszak@protonmail.com>
2024-02-11 20:31:08 +0100

    Initial commit

---
+++ src/main/java/com/poleszak/longest_substring/Main.java
@@ -0,0 +1,10 @@
+package com.poleszak.longest_substring;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BruteForceSubstringFinder bruteForceSubstringFinder = new BruteForceSubstringFinder();
+
+        int maxLengthBruteForceMethod = bruteForceSubstringFinder.lengthOfLongestSubstring("abcabcabcabcd");
+    }
+}
