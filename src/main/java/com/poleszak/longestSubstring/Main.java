02bf50e421329745f4ca3cac6742157a32d38121
Poleszak <poleszak@protonmail.com>
2024-02-12 22:47:56 +0100

    [Find Longest Substring] Call SpeedyFinder in Main

--- src/main/java/com/poleszak/longestSubstring/Main.java
+++ src/main/java/com/poleszak/longestSubstring/Main.java
@@ -2,18 +2,25 @@ package com.poleszak.longestSubstring;
 
 import com.poleszak.longestSubstring.tools.BruteForceSubstringFinder;
 import com.poleszak.longestSubstring.tools.FastSubstringFinder;
+import com.poleszak.longestSubstring.tools.SpeedySubstringFinder;
 
 public class Main {
 
     public static void main(String[] args) {
+        String str = "abcabcfdssdfsdfsadfasdfsadfabcabcd";
+
+
         BruteForceSubstringFinder bruteForceSubstringFinder = new BruteForceSubstringFinder();
         FastSubstringFinder fastSubstringFinder = new FastSubstringFinder();
+        SpeedySubstringFinder speedySubstringFinder = new SpeedySubstringFinder();
 
-        int maxLengthBruteForceMethod = bruteForceSubstringFinder.lengthOfLongestSubstring("abcabcfdssdfsdfsadfasdfsadfabcabcd");
-        int maxLengthFastMethod = fastSubstringFinder.lengthOfLongestSubstring("abcabcfdssdfsdfsadfasdfsadfabcabcd");
+        int maxLengthBruteForceMethod = bruteForceSubstringFinder.lengthOfLongestSubstring(str);
+        int maxLengthFastMethod = fastSubstringFinder.lengthOfLongestSubstring(str);
+        int maxLengthSpeedyMethod = speedySubstringFinder.lengthOfLongestSubstring(str);
 
 
         System.out.println(maxLengthBruteForceMethod);
         System.out.println(maxLengthFastMethod);
+        System.out.println(maxLengthSpeedyMethod);
     }
 }
