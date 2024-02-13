1d81cf351c61f9edbfbe574d291990d4a3c05844
Poleszak <poleszak@protonmail.com>
2024-02-13 12:14:11 +0100

    [Binary Search] BinarySearch

--- src/main/java/com/poleszak/binarySearch/Main.java
+++ src/main/java/com/poleszak/binarySearch/Main.java
@@ -1,7 +1,17 @@
 package com.poleszak.binarySearch;
 
+import com.poleszak.binarySearch.tools.BinarySearch;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("sdfsdfs");
+        int[] ints = {1, 2, 2, 3, 4, 5, 6, 7, 8};
+
+        int indexCustomMethod = BinarySearch.search(ints, 6);
+        int indexArraysMethod = Arrays.binarySearch(ints, 6);
+
+        System.out.println(indexCustomMethod);
+        System.out.println(indexArraysMethod);
     }
 }
--- src/main/java/com/poleszak/binarySearch/tools/BinarySearch.java
+++ src/main/java/com/poleszak/binarySearch/tools/BinarySearch.java
@@ -2,4 +2,23 @@ package com.poleszak.binarySearch.tools;
 
 public class BinarySearch {
 
+    public static int search(int[] numbers, int numberToFind) {
+        int low = 0;
+        int high = numbers.length - 1;
+
+        while (low <= high) {
+            int middleIndex = (low + high) / 2;
+            int middleNumber = numbers[middleIndex];
+
+            if (numberToFind == middleNumber) {
+                return middleIndex;
+            }
+            if (numberToFind < middleNumber) {
+                high = middleIndex - 1;
+            } else {
+                low = middleIndex + 1;
+            }
+        }
+        return -1;
+    }
 }
