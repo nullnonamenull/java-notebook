883b4c8561805bc93306623bc33974652ec29ba6
Piotras <poleszak@protonmail.com>
2024-03-02 15:57:56 +0100

    [Optional] OptionalLearn

--- src/main/java/com/poleszak/learnJava/OptionalLearn.java
+++ src/main/java/com/poleszak/learnJava/OptionalLearn.java
@@ -1,8 +1,34 @@
 package com.poleszak.learnJava;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
 public class OptionalLearn {
 
     public static void learnOptional() {
 
+        List<Optional<Integer>> optionalList = getRandomValue();
+        for (Optional<Integer> optionalI1 : optionalList) {
+            optionalI1.ifPresent(System.out::println);
+        }
+
+        System.out.println("\n");
+
+        for (Optional<Integer> optionalI1 : optionalList) {
+            System.out.println(optionalI1.orElse(100));
+        }
+    }
+
+    private static List<Optional<Integer>> getRandomValue() {
+        Random random = new Random();
+        List<Optional<Integer>> optionalIs = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int val = random.nextInt(1000);
+            optionalIs.add(val % 2 == 0 ? Optional.of(random.nextInt(101)) : Optional.empty());
+        }
+        return optionalIs;
     }
 }
