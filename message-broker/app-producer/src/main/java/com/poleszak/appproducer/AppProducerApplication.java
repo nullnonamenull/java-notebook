28e0c98822fe8fbdf521030da5fa85b941340487
Piotras <poleszak@protonmail.com>
2024-03-16 14:39:05 +0100

    [MessageBroker] Architecture

---
+++ message-broker/app-producer/src/main/java/com/poleszak/appproducer/AppProducerApplication.java
@@ -0,0 +1,13 @@
+package com.poleszak.appproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppProducerApplication.class, args);
+    }
+
+}
